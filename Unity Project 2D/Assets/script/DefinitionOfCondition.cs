642a5e0c4268a6f7b51469fb0b3cc0d28368859a
김지철 <shopopl@naver.com>
2017-06-05 19:06:57 +0900

    base
    base

---
+++ Unity Project 2D/Assets/script/DefinitionOfCondition.cs
@@ -0,0 +1,15 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+
+public class DefinitionOfCondition : MonoBehaviour {
+    
+
+    //private PlayerControll player;
+
+
+    //총알은 버튼을 손에서 땠을 때 발사가 되어야 함.
+
+    
+}
---
+++ Unity Project 2D/Assets/script/KeyStateManager.cs
@@ -0,0 +1,36 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class KeyStateManager : MonoBehaviour {
+
+    bool isPress;
+    bool isUp;
+
+	// Use this for initialization
+	void Start () {
+        isPress = false;
+        isUp = true;
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+
+    void KeyPress(KeyCode key)
+    {
+        if (Input.GetKey(key))
+        {
+
+        }
+    }
+
+    void KeyUp(KeyCode key)
+    {
+        if (Input.GetKey(key))
+        {
+
+        }
+    }
+}
